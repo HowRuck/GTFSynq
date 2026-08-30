e23f579ca9044f934f7ea1ab184613cdaa630df9
HowRuck <ertus123123@gmail.com>
2026-08-30 23:06:59 +0200

    build: consolidate per-module Dockerfiles into shared multi-stage Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,13 +4,8 @@ set -eu
 exec java \
   -XX:+UseG1GC \
   -XX:+UseStringDeduplication \
-  -XX:+UnlockExperimentalVMOptions \
   -XX:+UseCompactObjectHeaders \
-  -XX:+UseContainerSupport \
   -XX:MaxRAMPercentage=75.0 \
   -XX:+ExitOnOutOfMemoryError \
-  -Dserver.port=8888 \
   -Dspring.aot.enabled=true \
-  -Dhibernate.bytecode.use_reflection_optimizer=true \
-  -Djava.security.egd=file:/dev/./urandom \
   -jar /app/app.jar
